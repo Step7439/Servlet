48bbf69389320118614c1361ee3af36afd20396a
Step7439 <zamashikovs@mail.ru>
2022-10-30 20:56:29 +0900

    Edit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepository {
+  List<Post> posts = new CopyOnWriteArrayList<>();
+  private final AtomicLong atomicLong = new AtomicLong(1L);
+  public List<Post> all() {
+    return posts;
+  }
+
+  public Optional<Post> getById(long id) {
+    return posts.stream()
+            .filter(post -> post.getId() == id)
+            .findFirst();
+  }
+
+  public Post save(Post post) {
+      if (post.getId() == 0L){
+        post.setId(atomicLong.getAndIncrement());
+        posts.add(post);
+        return post;
+      }else {
+        final Post endPost = getById(post.getId()).orElseThrow(NotFoundException::new);
+        endPost.setContent(post.getContent());
+        return endPost;
+      }
+  }
+  public Post removeById(long id) {
+    final Post oldPost = getById(id).orElseThrow(NotFoundException::new);
+    posts.removeIf(post -> post.getId() == id);
+    return oldPost;
+  }
+}
